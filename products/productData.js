6609525eb009a10c690e25058feb616f186532de
Annalise Murphy <nali247@gmail.com>
2020-07-13 14:30:19 -0700

    static design / skeleton in place

---
+++ products/productData.js
@@ -0,0 +1,47 @@
+// export default
+
+//(Remember: the Add button is = product id!) 
+
+// product 1 name
+    // id
+    // name
+    // image
+    // description
+    // category
+    // price
+
+
+// product 2 name
+    // id
+    // name
+    // image
+    // description
+    // category
+    // price
+
+
+// product 3 name
+    // id
+    // name
+    // image
+    // description
+    // category
+    // price
+
+
+// product 4 name
+    // id
+    // name
+    // image
+    // description
+    // category
+    // price
+    
+    
+// product 5 name
+    // id
+    // name
+    // image
+    // description
+    // category
+    // price
