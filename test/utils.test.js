38083c25660b509ca1eea66f9df3c1f006640465
Annalise Murphy <nali247@gmail.com>
2020-07-14 16:22:49 -0700

    all prepped for testing - cart to be made after

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,20 +1,20 @@
-import { findById } from '../utils.js';
+// import { findById } from '../utils.js';
 
 
-const test = QUnit.test;
+// const test = QUnit.test;
 
-test('findById', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
-    const expected = 45.00;
-//price for items 1 - 5 - double on number 2
+// test('findById', function(assert) {
+//     //Arrange
+//     // Set up your parameters and expectations
+//     const expected = 45.00;
+// //price for items 1 - 5 - double on number 2
 
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const orderTotal = calcOrderTotal(cart, exoticFruits);
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+//     const orderTotal = calcOrderTotal(cart, exoticFruits);
 
-    //Assert
-    // Make assertions about what is expected valid result
-    assert.equal(orderTotal, expected);
-});
+//     //Assert
+//     // Make assertions about what is expected valid result
+//     assert.equal(orderTotal, expected);
+// });
 
